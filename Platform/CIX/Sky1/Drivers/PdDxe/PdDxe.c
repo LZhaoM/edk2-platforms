6c9215ccd786fb5e44f6d8388b56031db2f2d2d4
Zhaoming Luo <luozhaoming@radxa.com>
2025-11-07 17:52:00 +0800

    Add debug print

--- Platform/CIX/Sky1/Drivers/PdDxe/PdDxe.c
+++ Platform/CIX/Sky1/Drivers/PdDxe/PdDxe.c
@@ -16,6 +16,7 @@
 
 PD_DEV_INFO  *mPdDevList = NULL;
 UINT8        mPdDevCnt   = 0;
+UINT8 my_count = 0;
 
 CHAR8  *TypecModeStrs[] = {
   "TYPEC_STATE_SAFE", "TYPEC_STATE_USB",  "TYPEC_DP_STATE_C", "TYPEC_DP_STATE_D",
@@ -69,11 +70,11 @@ TypecPhyTimerCallback (
     }
   }
 
-  DEBUG ((
+  DebugPrint (
     DEBUG_INFO,
     "[Typec phy %d timer callback exit]\n\n\n",
     TypcPhyIdx
-    ));
+    );
 
   FreePool(Context);
 
@@ -93,13 +94,13 @@ UpdateTypecPhy (
   UINTN               PhyIndex         = 0;
   TYPEC_PHY_PROTOCOL  *TypecPhy;
 
-  DEBUG ((
+  DebugPrint (
     DEBUG_INFO,
     "Update Typec phy %d, Mode: %a, Orientation: %a\n",
     TypcPhyIdx,
     TypecModeStrs[PortState.Mode],
     TypecOrientationStrs[PortState.Orientation]
-    ));
+    );
 
   Status = gBS->LocateHandleBuffer (
                   ByProtocol,
@@ -127,11 +128,11 @@ UpdateTypecPhy (
 
   if (PhyIndex == PhyCount) {
     // no matched typc phy found
-    DEBUG ((
+    DebugPrint (
       DEBUG_INFO,
       "Typec phy %d is not supported\n",
       TypcPhyIdx
-      ));
+      );
     Status = EFI_UNSUPPORTED;
     goto exit;
   }
@@ -164,6 +165,8 @@ UpdateTypecPhy (
     goto exit;
   }
 
+  DebugPrint (DEBUG_INFO, "ReadyForIrq %d\n", ReadyForIrq);
+  DebugPrint (DEBUG_INFO, "TRUE is %d\n", TRUE);
   if (ReadyForIrq) {
     EFI_EVENT       TimerEvent;
     TYPEC_PHY_INFO  *PhyInfo = (TYPEC_PHY_INFO *)AllocatePool (sizeof (TYPEC_PHY_INFO));
@@ -178,12 +181,12 @@ UpdateTypecPhy (
                     &TimerEvent
                     );
     Status = gBS->SetTimer (TimerEvent, TimerRelative, PHY_SWITCH_DELAY_AFTER_PD_UPDATE);
-    DEBUG ((
+    DebugPrint (
       DEBUG_INFO,
       "Typec phy %d create timer callback %r\n",
       TypcPhyIdx,
       Status
-      ));
+      );
   }
 
 exit:
@@ -205,10 +208,10 @@ PdAlertCallBack (
   UINT8           *ConnectedIdx = AlertInfo->ConnectIdxs;
   UINT8           ConnectedCnt  = AlertInfo->ConnectCnt;
 
-  DEBUG ((
+  DebugPrint (
     DEBUG_INFO,
     "\n\n[Pd Alert Interrupt Enter]\n"
-    ));
+    );
 
   for (UINT8 Index = 0; Index < ConnectedCnt; Index++) {
     UINT8        PdDevIdx = ConnectedIdx[Index];
@@ -229,18 +232,25 @@ PdAlertCallBack (
         Status
         );
     } else {
-      DEBUG ((
+      DebugPrint (
         DEBUG_INFO,
         "Update Pd Device %d state, Mode: %a, Orientation: %a\n",
         PdDevIdx,
         TypecModeStrs[PdDev->TypecPortState.Mode],
         TypecOrientationStrs[PdDev->TypecPortState.Orientation]
-        ));
-      UpdateTypecPhy (
-        PdDev->PhyControlIndex,
-        PdDev->TypecPhyReadyForIRQ,
-        PdDev->TypecPortState
         );
+      if (PdDev->TypecPortState.Mode != TYPEC_STATE_USB) {
+	if (my_count >= 1) {
+          DebugPrint (DEBUG_INFO, "UpdateTypecPhy!!!\n");
+          UpdateTypecPhy (
+            PdDev->PhyControlIndex,
+            PdDev->TypecPhyReadyForIRQ,
+            PdDev->TypecPortState
+            );
+	} else {
+	  my_count += 1;
+	}
+      }
     }
 
     PdExitAlertInterrupt (PdDevIdx);
@@ -258,17 +268,17 @@ PdAlertCallBack (
   } while (RetryCnt < ALERT_PIN_POLL_MAX_COUNT);
 
   if(RetryCnt == ALERT_PIN_POLL_MAX_COUNT) {
-    DEBUG ((
+    DebugPrint (
         DEBUG_INFO,
         "Pd alert interrupt (gpio [%d]) not cleared in time\n",
         AlertInfo->AlertPin
-        ));
+        );
   }
 
-  DEBUG ((
+  DebugPrint (
     DEBUG_INFO,
     "[Pd Alert Interrupt Exit]\n\n\n"
-    ));
+    );
 }
 
 VOID
@@ -385,6 +395,8 @@ SetPhyReadyForAlertIRQ (
     return EFI_UNSUPPORTED;
   }
 
+//  gBS->Stall(10000000);
+  DebugPrint (DEBUG_INFO, "SetPhyReadyForAlertIRQ!!!\n");
   mPdDevList[PdDevIndex].TypecPhyReadyForIRQ = TRUE;
 
   return EFI_SUCCESS;
@@ -429,16 +441,16 @@ RegisterAlertTimerCallback (
 
   SortEnabledAlertPin (mPdDevList, mPdDevCnt, &EnabledPins, &EnabledCnt);
 
-  DEBUG ((DEBUG_INFO, "Enabled alert pin nums:"));
+  DebugPrint (DEBUG_INFO, "Enabled alert pin nums:");
   if (EnabledCnt) {
     for ( UINT8 i = 0; i < EnabledCnt; i++) {
-      DEBUG ((DEBUG_INFO, " %d", EnabledPins[i]));
+      DebugPrint (DEBUG_INFO, " %d", EnabledPins[i]);
     }
   } else {
-    DEBUG ((DEBUG_INFO, " not found"));
+    DebugPrint (DEBUG_INFO, " not found");
   }
 
-  DEBUG ((DEBUG_INFO, "\n"));
+  DebugPrint (DEBUG_INFO, "\n");
 
   for (Index = 0; Index < EnabledCnt; Index++) {
     UINT8  *ConnectedIdx;
@@ -446,16 +458,16 @@ RegisterAlertTimerCallback (
 
     CollectConnectedDeviceIndex (mPdDevList, mPdDevCnt, EnabledPins[Index], &ConnectedIdx, &ConnectedCnt);
 
-    DEBUG ((DEBUG_INFO, "Alert Pin Num %d connected pd device indexes:", EnabledPins[Index]));
+    DebugPrint (DEBUG_INFO, "Alert Pin Num %d connected pd device indexes:", EnabledPins[Index]);
     if (ConnectedCnt) {
       for (UINT8 i = 0; i < ConnectedCnt; i++) {
-        DEBUG ((DEBUG_INFO, " %d", ConnectedIdx[i]));
+        DebugPrint (DEBUG_INFO, " %d", ConnectedIdx[i]);
       }
     } else {
-      DEBUG ((DEBUG_INFO, " not found"));
+      DebugPrint (DEBUG_INFO, " not found");
     }
 
-    DEBUG ((DEBUG_INFO, "\n"));
+    DebugPrint (DEBUG_INFO, "\n");
 
     if (ConnectedCnt > 0) {
       EFI_EVENT TimerEvent;
@@ -472,7 +484,7 @@ RegisterAlertTimerCallback (
                     &TimerEvent
                     );
       Status = gBS->SetTimer (TimerEvent, TimerRelative, ALERT_TIMER_CALLBACK_INTERVAL);
-      DEBUG ((DEBUG_INFO, "%a: register alert pin num %d pd timer callback status: %r\n", __FUNCTION__, EnabledPins[Index], Status));
+      DebugPrint (DEBUG_INFO, "%a: register alert pin num %d pd timer callback status: %r\n", __FUNCTION__, EnabledPins[Index], Status);
     }
   }
 
@@ -505,9 +517,11 @@ PdDxeEntryPoint (
   UINT8       *pTpyecDftModes        = PcdGetPtr (PcdTypecPortDefaultModes);
   UINT8       *pTpyecDftOrientations = PcdGetPtr (PcdTypecPortDefaultOrientations);
 
+//  gBS->Stall(10000000);
   POST_CODE (PdDxeStart);
 
   mPdDevCnt  = PcdGet8 (PcdPdDevCount);
+  DebugPrint (DEBUG_INFO, "PdDevCnt is %d\n", mPdDevCnt);
   mPdDevList = (PD_DEV_INFO *)AllocatePool (mPdDevCnt * sizeof (PD_DEV_INFO));
 
   for (UINT8 PdDevIdx = 0; PdDevIdx < mPdDevCnt; PdDevIdx++) {
@@ -524,11 +538,11 @@ PdDxeEntryPoint (
                  &PdDev->TypecPortState
                  );
       if (EFI_ERROR (Status)) {
-        DEBUG ((
+        DebugPrint (
           DEBUG_INFO,
           "Pd Device %d update port state fail, set default state\n",
           PdDevIdx
-          ));
+          );
 
         // get port state fail, set default state
         PdDev->TypecPortState.Mode        = pTpyecDftModes[PdDevIdx];
@@ -536,11 +550,11 @@ PdDxeEntryPoint (
       }
     } else {
       // pd device is invalid
-      DEBUG ((
+      DebugPrint (
         DEBUG_INFO,
         "Pd Device %d is invalid, set default state\n",
         PdDevIdx
-        ));
+        );
 
       // no pd device connected
       PdDev->TypecPortState.Mode        = pTpyecDftModes[PdDevIdx];
@@ -589,6 +603,7 @@ PdDxeEntryPoint (
      return Status;
   }
 
+//  gBS->Stall(10000000);
   POST_CODE (PdDxeEnd);
 
   return EFI_SUCCESS;
